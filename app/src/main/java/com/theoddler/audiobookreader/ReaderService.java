860c1fb90026f87f088ad6f8c2f68c281f4b9905
Pablo Bollansée <pablo.bollansee@gmail.com>
2015-11-07 23:09:06 +0100

    Added book progress info and a start for the reader service code.

---
+++ app/src/main/java/com/theoddler/audiobookreader/ReaderService.java
@@ -0,0 +1,73 @@
+package com.theoddler.audiobookreader;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.os.PowerManager;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Pablo on 07/11/15.
+ */
+public class ReaderService extends Service implements
+        MediaPlayer.OnPreparedListener,
+        MediaPlayer.OnErrorListener,
+        MediaPlayer.OnCompletionListener {
+
+    private MediaPlayer player;
+    private Book book;
+    private Book.Progress progress;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        player = new MediaPlayer();
+        initPlayer();
+    }
+
+    private void initPlayer() {
+        player.setWakeMode(getApplicationContext(), PowerManager.PARTIAL_WAKE_LOCK);
+        player.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+        player.setOnPreparedListener(this);
+        player.setOnCompletionListener(this);
+        player.setOnErrorListener(this);
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+        this.progress = book.getStartProgress();
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+
+    }
+
+    @Override
+    public boolean onError(MediaPlayer mp, int what, int extra) {
+        return false;
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+
+    }
+
+    /**
+     * The binder for this reader.
+     */
+    public class Binder extends android.os.Binder {
+        ReaderService getService() {
+            return ReaderService.this;
+        }
+    }
+}
